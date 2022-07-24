824d0817942bf9626eda84143b03431c0d5d4865
Framsb <frambesbarrios@gmail.com>
2022-07-24 02:58:21 +0000

    av 1

--- src/main.py
+++ src/main.py
@@ -2,13 +2,15 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+import json
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planets, Characters, Favorite_Character
+import requests
 #from models import Person
 
 app = Flask(__name__)
@@ -30,15 +32,81 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
+@app.route('/users', methods=['GET'])
 def handle_hello():
-
+    user = User.query.all()
+    all_user = list(map(lambda x: x.serialize(), user))
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "message": all_user
     }
-
     return jsonify(response_body), 200
 
+
+@app.route('/users/<int:id>', methods=['GET'])
+def handle_user(id):
+    user = User.query.get(id)
+    if user is None:
+        return "Not found",
+    return jsonify(user.serialize()), 200
+    
+
+@app.route('/planets', methods=['GET'])
+def handle_planets():
+    planet = Planets.query.all()
+    allplanets = list(map(lambda x: x.serialize(),planet))
+    response_body = {
+        "planets": allplanets
+        }
+    return jsonify(response_body), 200
+
+@app.route('/characters', methods=['GET'])
+def handle_characters():
+    character = Characters.query.all()
+    all_characters = list(map(lambda x: x.serialize(),character))
+    response_body = {
+        "characters": all_characters
+        }
+    return jsonify(response_body), 200
+
+@app.route('/characters/<int:id>')
+def handle_character(id):
+    character = Characters.query.get(id)
+    if character is None:
+        return "Not Found", 404
+    return jsonify(character.serialize())
+
+@app.route('/planets/<int:id>')
+def handle_planet(id):
+    planet = Planets.query.get(id)
+    if planet is None:
+        return "Not Found", 404
+    return jsonify(planet.serialize())
+
+@app.route('/users/<int:userid>/favorite_character', methods=['GET'])
+def handle_Favorites(userid):
+    user_favorites = Favorite_Character.query.filter_by(userid= userid)
+    all_favorites = list(map(lambda x: x.serialize(), user_favorites))
+    return jsonify(all_favorites)
+    
+@app.route('/users/<int:userid>/favorite_character/<int:characterid>', methods=['POST','DELETE'])
+def handle_favorite(userid,characterid):
+    user = User.query.get(userid)
+    if user is None:
+        return "Not Found"
+    if request.method == 'POST':
+        FavCharacter = Favorite_Character(userid = userid , characterid = characterid)
+        db.session.add(FavCharacter)
+        db.session.commit()
+        return jsonify(FavCharacter.serialize()), 201
+    else:
+        FavDelete = Favorite_Character.query.filter_by(userid = userid ,characterid=characterid).one_or_none()
+        if FavDelete is None:
+                return('not found in data base')
+        else:
+            db.session.delete(FavDelete)
+            db.session.commit()
+            return "Eliminated", 200
+        
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -7,6 +7,8 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    favorite_character = db.relationship('Favorite_Character', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -14,6 +16,60 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+    
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(60), nullable=False)
+    gravity = db.Column(db.Integer, nullable=False)
+    terrain = db.Column(db.String(60), nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    climate = db.Column(db.String(60), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gravity":self.gravity,
+            "terrain": self.terrain,
+            "population": self.population,
+            "climate": self.climate,
+        }
+        
+class Characters(db.Model):
+    __tablename__= "characters"
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(60), nullable=False)
+    lastname = db.Column(db.String(60), nullable=False)
+    gender = db.Column(db.String(60),nullable=False)
+    eyes = db.Column(db.String(60), nullable=False)
+    hair = db.Column(db.String(60),nullable=False)
+    birth_year = db.Column(db.String(60), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "lastname":self.lastname,
+            "gender": self.gender,
+            "eyes": self.eyes,
+            "hair": self.hair,
+            "birth_year": self.birth_year,
+        }
+        
+class Favorite_Character(db.Model):
+    __tablename__="favorite_character"
+    id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.Integer, db.ForeignKey('user.id'))
+    characterid = db.Column(db.Integer, db.ForeignKey('characters.id'))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "userid": self.userid,
+            "characterid": self.characterid,
         }
